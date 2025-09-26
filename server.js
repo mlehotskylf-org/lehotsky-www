6409ade7e45919dd3ea61ad4c31208f79afdfb33
Michal Lehotsky <mlehotsky@linuxfoundation.org>
2025-09-25 22:55:15 -0700

    Updating Intercom App ID

--- server.js
+++ server.js
@@ -26,13 +26,13 @@ app.get("/", (req, res) => {
 <script>
   window.intercomSettings = {
     api_base: "https://api-iam.intercom.io",
-    app_id: "mxl90k6y",
+    app_id: "rr2dqmfv",
   };
 </script>
 
 <script>
-  // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/mxl90k6y'
-  (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/mxl90k6y';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+  // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/rr2dqmfv'
+  (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/rr2dqmfv';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
 </script>`);
 });
 
