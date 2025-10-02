015022dcebb3b1c46d148ed0b7ab4267d949f010
Michal Lehotsky <mlehotsky@linuxfoundation.org>
2025-10-01 21:59:39 -0700

    Adding listener and once authentication in separate tab is done then update Intercom

--- server.js
+++ server.js
@@ -31,6 +31,24 @@ app.get("/", (req, res) => {
     // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/rr2dqmfv'
     (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/rr2dqmfv';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
   </script>
+
+  <script>
+    // Listen for login success from popup/new tab
+    window.addEventListener('message', function(event) {
+      // Security: only accept messages from auth domain
+      if (event.origin !== 'https://intercom-auth.lehotsky.net') {
+        return;
+      }
+
+      if (event.data.type === 'intercom_login_success') {
+        console.log('Login successful in popup, refreshing Intercom');
+        // Refresh Intercom to pick up new cookies
+        if (window.Intercom) {
+          window.Intercom('update');
+        }
+      }
+    });
+  </script>
 </body>
 </html>`);
 });
@@ -56,6 +74,24 @@ app.get("/members", (req, res) => {
   <script>
     // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/rr2dqmfv'
     (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/rr2dqmfv';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+  </script>
+
+  <script>
+    // Listen for login success from popup/new tab
+    window.addEventListener('message', function(event) {
+      // Security: only accept messages from auth domain
+      if (event.origin !== 'https://intercom-auth.lehotsky.net') {
+        return;
+      }
+
+      if (event.data.type === 'intercom_login_success') {
+        console.log('Login successful in popup, refreshing Intercom');
+        // Refresh Intercom to pick up new cookies
+        if (window.Intercom) {
+          window.Intercom('update');
+        }
+      }
+    });
   </script>
 </body>
 </html>`);
